e48967b9a42eefb5b674a7fa9ea7f36f49094c01
Earth1283 <joseph_cio@hotmail.com>
2026-04-19 10:31:04 +0800

    docs: add gradle performance optimization design spec

---
+++ src/main/kotlin/io/github/earth1283/dBManager/DBManager.kt
@@ -0,0 +1,14 @@
+package io.github.Earth1283.dBManager
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DBManager : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/io/github/earth1283/dBManager/DBManagerLoader.kt
@@ -0,0 +1,11 @@
+package io.github.Earth1283.dBManager
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder
+import io.papermc.paper.plugin.loader.PluginLoader
+
+internal class DBManagerLoader : PluginLoader {
+
+    override fun classloader(builder: PluginClasspathBuilder) {
+        // Add dynamically loaded libraries here
+    }
+}
